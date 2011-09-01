902d53c5546152a34d8a04a2e2871e33b1a43407
Michael Landis <landis.michael@gmail.com>
2011-09-01 07:55:04 -0400

    - added exception for BookNotFound

---
+++ src/com/hintersphere/booklogger/BookNotFoundException.java
@@ -0,0 +1,29 @@
+package com.hintersphere.booklogger;
+
+public class BookNotFoundException extends Exception {
+
+	private static final long serialVersionUID = -3833112083342218653L;
+
+	public BookNotFoundException() {
+		super();
+		// TODO Auto-generated constructor stub
+	}
+
+	public BookNotFoundException(String detailMessage, Throwable throwable) {
+		super(detailMessage, throwable);
+		// TODO Auto-generated constructor stub
+	}
+
+	public BookNotFoundException(String detailMessage) {
+		super(detailMessage);
+		// TODO Auto-generated constructor stub
+	}
+
+	public BookNotFoundException(Throwable throwable) {
+		super(throwable);
+		// TODO Auto-generated constructor stub
+	}
+
+	
+	
+}
