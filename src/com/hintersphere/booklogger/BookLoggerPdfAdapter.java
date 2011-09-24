8c5f2b3449f85c7ae1b9926472d48254ceedf6dd
Michael Landis <landis.michael@gmail.com>
2011-09-24 12:41:02 -0400

    in the process of converting pdf libraries. removed itext and commented out a bunch of code for now. added source code for apwlibrary

--- src/com/hintersphere/booklogger/BookLoggerPdfAdapter.java
+++ src/com/hintersphere/booklogger/BookLoggerPdfAdapter.java
@@ -5,20 +5,12 @@ import java.io.FileOutputStream;
 import java.text.SimpleDateFormat;
 import java.util.Date;
 
+import crl.android.pdfwriter.PDFWriter;
+import crl.android.pdfwriter.PaperSize;
+
 import android.content.Context;
 import android.database.Cursor;
 import android.os.Environment;
-
-import com.itextpdf.text.BaseColor;
-import com.itextpdf.text.Document;
-import com.itextpdf.text.DocumentException;
-import com.itextpdf.text.Element;
-import com.itextpdf.text.Font;
-import com.itextpdf.text.Phrase;
-import com.itextpdf.text.Font.FontFamily;
-import com.itextpdf.text.pdf.PdfPCell;
-import com.itextpdf.text.pdf.PdfPTable;
-import com.itextpdf.text.pdf.PdfWriter;
 
 public class BookLoggerPdfAdapter {
 	
@@ -26,7 +18,6 @@ public class BookLoggerPdfAdapter {
 	private static final SimpleDateFormat DATE_FORMAT_PDF = new SimpleDateFormat("MM/dd/yy");
 
 	private Context mCtx;
-	private Document mDocument;
 		
 	public BookLoggerPdfAdapter(Context ctx) {
 
@@ -43,159 +34,161 @@ public class BookLoggerPdfAdapter {
 		File outputFile = null;
 		File sdDir = Environment.getExternalStorageDirectory(); 
 		outputFile = new File(sdDir, "/booklog.pdf");
-
-		mDocument = new Document();
-		
-		try {
-			PdfWriter.getInstance(mDocument, new FileOutputStream(outputFile));
-		} catch (Exception e) {
-			throw new BookLoggerException("Error creating output stream for pdf email.", e);			
-		}
-		mDocument.open();
-		
-		// add metadata
-		mDocument.addTitle(title);
-		mDocument.addSubject(subject);
-		mDocument.addKeywords(mCtx.getString(R.string.pdf_doc_keywords));
-		mDocument.addAuthor(mCtx.getString(R.string.pdf_doc_author));
-		mDocument.addCreator(mCtx.getString(R.string.pdf_doc_creator));
-
-		// Summary table for entering name and teacher's name
-		float[] colswidth = {20f, 70f};
-		PdfPTable table = new PdfPTable(colswidth);
-		table.setHorizontalAlignment(Element.ALIGN_LEFT);
-		table.setWidthPercentage(70f);
-		table.setSpacingAfter(18);
-		
-		// Font for styling summary cells
-		Font summaryFont = new Font(FontFamily.HELVETICA, 18, Font.BOLD);
-		PdfPCell cell = new PdfPCell(new Phrase(mCtx.getString(R.string.pdf_summary_instructor),
-				summaryFont));
-		cell.setBorder(PdfPCell.NO_BORDER);
-		cell.setHorizontalAlignment(Element.ALIGN_LEFT);		
-		table.addCell(cell);
-		PdfPCell summaryValueCell = new PdfPCell(new Phrase("", summaryFont));
-		summaryValueCell.setBorder(PdfPCell.BOTTOM);
-		summaryValueCell.setHorizontalAlignment(Element.ALIGN_LEFT);
-		table.addCell(summaryValueCell);
-		cell.setPhrase(new Phrase(mCtx.getString(R.string.pdf_summary_student), summaryFont));
-		table.addCell(cell);		
-		table.addCell(summaryValueCell);		
-		cell.setPhrase(new Phrase(mCtx.getString(R.string.pdf_summary_total) + " "
-				+ cursor.getCount(), summaryFont));
-		cell.setColspan(2);
-		cell.setHorizontalAlignment(Element.ALIGN_LEFT);
-		
-		table.addCell(cell);
-
-		try {
-			mDocument.add(table);
-		} catch (DocumentException e) {
-			throw new BookLoggerException("Could not add summary table to the document.", e);
-		}
+		PDFWriter pdfWriter = new PDFWriter(PaperSize.LETTER_WIDTH, PaperSize.LETTER_WIDTH);
 
 		
-		// kick off the table
-		float[] colswidth2 = {5f, 10f, 40f, 20f, 15f, 10f};
-		table = new PdfPTable(colswidth2);
-		table.setHorizontalAlignment(Element.ALIGN_LEFT);
-		table.setWidthPercentage(100f);
-		table.setSpacingAfter(8);
-		
-		// Font for styling header cells
-		Font headerFont = new Font(FontFamily.HELVETICA, 12, Font.BOLD);
+//		// add metadata
+//		mDocument.addTitle(title);
+//		mDocument.addSubject(subject);
+//		mDocument.addKeywords(mCtx.getString(R.string.pdf_doc_keywords));
+//		mDocument.addAuthor(mCtx.getString(R.string.pdf_doc_author));
+//		mDocument.addCreator(mCtx.getString(R.string.pdf_doc_creator));
+//
+//		// Summary table for entering name and teacher's name
+//		float[] colswidth = {20f, 70f};
+//		PdfPTable table = new PdfPTable(colswidth);
+//		table.setHorizontalAlignment(Element.ALIGN_LEFT);
+//		table.setWidthPercentage(70f);
+//		table.setSpacingAfter(18);
+//		
+//		// Font for styling summary cells
+//		Font summaryFont = new Font(FontFamily.HELVETICA, 18, Font.BOLD);
+//		PdfPCell cell = new PdfPCell(new Phrase(mCtx.getString(R.string.pdf_summary_instructor),
+//				summaryFont));
+//		cell.setBorder(PdfPCell.NO_BORDER);
+//		cell.setHorizontalAlignment(Element.ALIGN_LEFT);		
+//		table.addCell(cell);
+//		PdfPCell summaryValueCell = new PdfPCell(new Phrase("", summaryFont));
+//		summaryValueCell.setBorder(PdfPCell.BOTTOM);
+//		summaryValueCell.setHorizontalAlignment(Element.ALIGN_LEFT);
+//		table.addCell(summaryValueCell);
+//		cell.setPhrase(new Phrase(mCtx.getString(R.string.pdf_summary_student), summaryFont));
+//		table.addCell(cell);		
+//		table.addCell(summaryValueCell);		
+//		cell.setPhrase(new Phrase(mCtx.getString(R.string.pdf_summary_total) + " "
+//				+ cursor.getCount(), summaryFont));
+//		cell.setColspan(2);
+//		cell.setHorizontalAlignment(Element.ALIGN_LEFT);
+//		
+//		table.addCell(cell);
+//
+//		try {
+//			mDocument.add(table);
+//		} catch (DocumentException e) {
+//			throw new BookLoggerException("Could not add summary table to the document.", e);
+//		}
+//
+//		
+//		// kick off the table
+//		float[] colswidth2 = {5f, 10f, 40f, 20f, 15f, 10f};
+//		table = new PdfPTable(colswidth2);
+//		table.setHorizontalAlignment(Element.ALIGN_LEFT);
+//		table.setWidthPercentage(100f);
+//		table.setSpacingAfter(8);
+//		
+//		// Font for styling header cells
+//		Font headerFont = new Font(FontFamily.HELVETICA, 12, Font.BOLD);
+//
+//		// Font for styling list cells
+//		Font listFont = new Font(FontFamily.HELVETICA, 10, Font.NORMAL);
+//
+//		cell = new PdfPCell(new Phrase(mCtx.getString(R.string.pdf_col_num), headerFont));
+//		cell.setHorizontalAlignment(Element.ALIGN_CENTER);
+//		cell.setVerticalAlignment(Element.ALIGN_MIDDLE);
+//		cell.setBackgroundColor(BaseColor.LIGHT_GRAY);
+//		cell.setBorderColor(BaseColor.LIGHT_GRAY);
+//		table.addCell(cell);
+//
+//		cell.setPhrase(new Phrase(mCtx.getString(R.string.pdf_col_date), headerFont));
+//		table.addCell(cell);
+//
+//		cell.setPhrase(new Phrase(mCtx.getString(R.string.pdf_col_title), headerFont));
+//		table.addCell(cell);
+//
+//		cell.setPhrase(new Phrase(mCtx.getString(R.string.pdf_col_author), headerFont));
+//		table.addCell(cell);
+//
+//		cell.setPhrase(new Phrase(mCtx.getString(R.string.pdf_col_activity), headerFont));
+//		table.addCell(cell);
+//
+//		cell.setPhrase(new Phrase(mCtx.getString(R.string.pdf_col_initials), headerFont));
+//		table.addCell(cell);
+//
+//		table.setHeaderRows(1);
+//		cell = new PdfPCell();
+//		cell.setHorizontalAlignment(Element.ALIGN_LEFT);
+//		cell.setBorderColor(BaseColor.LIGHT_GRAY);
+//		
+//		while (cursor.moveToNext()) {
+//
+//			// assemble the components of the record...
+//			cell.setPhrase(new Phrase("" + (cursor.getPosition() + 1), listFont));
+//			table.addCell(cell); // 0 indexed
+//			cell.setPhrase(new Phrase(formatDate(cursor.getString(cursor
+//					.getColumnIndex(BookLoggerDbAdapter.DB_COL_CREATEDT))), listFont));
+//			cell.setHorizontalAlignment(Element.ALIGN_CENTER);			
+//			table.addCell(cell);
+//			
+//			cell.setPhrase(new Phrase(cursor.getString(cursor
+//					.getColumnIndex(BookLoggerDbAdapter.DB_COL_TITLE)), listFont));
+//			cell.setHorizontalAlignment(Element.ALIGN_LEFT);			
+//			table.addCell(cell);
+//			
+//			cell.setPhrase(new Phrase(cursor
+//					.getString(cursor.getColumnIndex(BookLoggerDbAdapter.DB_COL_AUTHOR)), listFont));
+//			table.addCell(cell);
+//			
+//			switch (cursor.getInt(cursor.getColumnIndex(BookLoggerDbAdapter.DB_COL_ACTIVITY))) {
+//			case BookLoggerDbAdapter.DB_ACTIVITY_CHILD_READ:
+//				cell.setPhrase(new Phrase(mCtx.getString(R.string.context_menu_childread), listFont));
+//				break;
+//			case BookLoggerDbAdapter.DB_ACTIVITY_PARENT_READ:
+//				cell.setPhrase(new Phrase(mCtx.getString(R.string.context_menu_parentread), listFont));
+//				break;
+//			case BookLoggerDbAdapter.DB_ACTIVITY_CHILD_PARENT_READ:
+//				cell.setPhrase(new Phrase(mCtx.getString(R.string.context_menu_parentchildread), listFont));
+//				break;
+//			}
+//
+//			table.addCell(cell);
+//
+//			// empty cell for the initials
+//			cell.setPhrase(new Phrase("", listFont));
+//			table.addCell(cell);			
+//		}		
+//
+//		try {
+//			mDocument.add(table);
+//		} catch (DocumentException e) {
+//			throw new BookLoggerException("Could not add list table to the document.", e);
+//		}
+//
+//		// Font for styling list cells
+//		Font footerFont = new Font(FontFamily.HELVETICA, 8, Font.ITALIC);
+//		cell = new PdfPCell(new Phrase(mCtx.getString(R.string.pdf_footer_tagline), footerFont));
+//		cell.setBorder(PdfPCell.NO_BORDER);
+//		cell.setHorizontalAlignment(Element.ALIGN_RIGHT);
+//
+//		table = new PdfPTable(1);
+//		table.setHorizontalAlignment(Element.ALIGN_RIGHT);
+//		table.setWidthPercentage(100f);
+//		table.addCell(cell);
+//		try {
+//			mDocument.add(table);
+//		} catch (DocumentException e) {
+//			throw new BookLoggerException("Could not add footer table to the document.", e);
+//		}
+//		
+//		mDocument.close();
+//		
+//        try {
+//        	FileOutputStream pdfFile = new FileOutputStream(newFile);
+//        	pdfFile.write(pdfContent.getBytes(encoding));
+//            pdfFile.close();
+//        } catch(FileNotFoundException e) {
+//        	//
+//        }
 
-		// Font for styling list cells
-		Font listFont = new Font(FontFamily.HELVETICA, 10, Font.NORMAL);
-
-		cell = new PdfPCell(new Phrase(mCtx.getString(R.string.pdf_col_num), headerFont));
-		cell.setHorizontalAlignment(Element.ALIGN_CENTER);
-		cell.setVerticalAlignment(Element.ALIGN_MIDDLE);
-		cell.setBackgroundColor(BaseColor.LIGHT_GRAY);
-		cell.setBorderColor(BaseColor.LIGHT_GRAY);
-		table.addCell(cell);
-
-		cell.setPhrase(new Phrase(mCtx.getString(R.string.pdf_col_date), headerFont));
-		table.addCell(cell);
-
-		cell.setPhrase(new Phrase(mCtx.getString(R.string.pdf_col_title), headerFont));
-		table.addCell(cell);
-
-		cell.setPhrase(new Phrase(mCtx.getString(R.string.pdf_col_author), headerFont));
-		table.addCell(cell);
-
-		cell.setPhrase(new Phrase(mCtx.getString(R.string.pdf_col_activity), headerFont));
-		table.addCell(cell);
-
-		cell.setPhrase(new Phrase(mCtx.getString(R.string.pdf_col_initials), headerFont));
-		table.addCell(cell);
-
-		table.setHeaderRows(1);
-		cell = new PdfPCell();
-		cell.setHorizontalAlignment(Element.ALIGN_LEFT);
-		cell.setBorderColor(BaseColor.LIGHT_GRAY);
-		
-		while (cursor.moveToNext()) {
-
-			// assemble the components of the record...
-			cell.setPhrase(new Phrase("" + (cursor.getPosition() + 1), listFont));
-			table.addCell(cell); // 0 indexed
-			cell.setPhrase(new Phrase(formatDate(cursor.getString(cursor
-					.getColumnIndex(BookLoggerDbAdapter.DB_COL_CREATEDT))), listFont));
-			cell.setHorizontalAlignment(Element.ALIGN_CENTER);			
-			table.addCell(cell);
-			
-			cell.setPhrase(new Phrase(cursor.getString(cursor
-					.getColumnIndex(BookLoggerDbAdapter.DB_COL_TITLE)), listFont));
-			cell.setHorizontalAlignment(Element.ALIGN_LEFT);			
-			table.addCell(cell);
-			
-			cell.setPhrase(new Phrase(cursor
-					.getString(cursor.getColumnIndex(BookLoggerDbAdapter.DB_COL_AUTHOR)), listFont));
-			table.addCell(cell);
-			
-			switch (cursor.getInt(cursor.getColumnIndex(BookLoggerDbAdapter.DB_COL_ACTIVITY))) {
-			case BookLoggerDbAdapter.DB_ACTIVITY_CHILD_READ:
-				cell.setPhrase(new Phrase(mCtx.getString(R.string.context_menu_childread), listFont));
-				break;
-			case BookLoggerDbAdapter.DB_ACTIVITY_PARENT_READ:
-				cell.setPhrase(new Phrase(mCtx.getString(R.string.context_menu_parentread), listFont));
-				break;
-			case BookLoggerDbAdapter.DB_ACTIVITY_CHILD_PARENT_READ:
-				cell.setPhrase(new Phrase(mCtx.getString(R.string.context_menu_parentchildread), listFont));
-				break;
-			}
-
-			table.addCell(cell);
-
-			// empty cell for the initials
-			cell.setPhrase(new Phrase("", listFont));
-			table.addCell(cell);			
-		}		
-
-		try {
-			mDocument.add(table);
-		} catch (DocumentException e) {
-			throw new BookLoggerException("Could not add list table to the document.", e);
-		}
-
-		// Font for styling list cells
-		Font footerFont = new Font(FontFamily.HELVETICA, 8, Font.ITALIC);
-		cell = new PdfPCell(new Phrase(mCtx.getString(R.string.pdf_footer_tagline), footerFont));
-		cell.setBorder(PdfPCell.NO_BORDER);
-		cell.setHorizontalAlignment(Element.ALIGN_RIGHT);
-
-		table = new PdfPTable(1);
-		table.setHorizontalAlignment(Element.ALIGN_RIGHT);
-		table.setWidthPercentage(100f);
-		table.addCell(cell);
-		try {
-			mDocument.add(table);
-		} catch (DocumentException e) {
-			throw new BookLoggerException("Could not add footer table to the document.", e);
-		}
-		
-		mDocument.close();
 		return outputFile;
 	}
 	
