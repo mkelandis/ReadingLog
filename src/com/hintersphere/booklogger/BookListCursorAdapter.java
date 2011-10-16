f9bda6da9676fe357fafd6d61b0284daf223a9d7
Michael Landis <landis.michael@gmail.com>
2011-10-15 20:10:32 -0400

    changed out icons

--- src/com/hintersphere/booklogger/BookListCursorAdapter.java
+++ src/com/hintersphere/booklogger/BookListCursorAdapter.java
@@ -59,7 +59,7 @@ public class BookListCursorAdapter extends CursorAdapter {
 	
 	private void init(Context context) {		  
         BitmapManager.INSTANCE.setPlaceholder(BitmapFactory.decodeResource(  
-                context.getResources(), R.drawable.launcherico));  
+                context.getResources(), R.drawable.defbookcover));  
 	}
 	
 	
